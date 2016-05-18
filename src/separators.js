5da62235b17b88d53db382859f524f2fe62f70b5
wyb1995 <wy_b1995@163.com>
2016-05-18 13:43:20 +0800

    ture

--- src/separators.js
+++ src/separators.js
@@ -2,7 +2,7 @@
 
 function thousands_separators(num) {
     num = num + '';
-    var reg = /(-?\d+)(\d{3})/;
+    var reg = /(-?\d+)(\d{3})/g;
 
     if(reg.test(num)){
         num = num.replace(reg, '$1,$2');
