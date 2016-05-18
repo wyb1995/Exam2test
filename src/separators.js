b2cb76accd5b3280685120a8997c1b28711bf29b
wyb1995 <wy_b1995@163.com>
2016-05-18 13:30:54 +0800

    aa

--- src/separators.js
+++ src/separators.js
@@ -1,12 +1,9 @@
 'use strict';
 
 function thousands_separators(num) {
-    if(!/^(\+|-)?(\d+)(\.\d+)?$/.test(num)){
-        return num;
-    }
     var a = RegExp.$1,b = RegExp.$2,c = RegExp.$3;
     var re = new RegExp().compile("(\\d)(\\d{3})(,|$)");
-    while(re.test(b)){
+    while(re.num(b)){
         b = b.replace(re,"$1,$2$3");
     }
     return a +""+ b +""+ c;
